783e8c587e0197a563716b31c450dfe42c9a611a
bhig-sheydi <bhigsheydi@gmail.com>
2024-03-04 15:07:30 -0800

    hero commit

--- brekete-master/src/Components/Home/Banner.jsx
+++ brekete-master/src/Components/Home/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../Home/Home.css"
 import { useNavigate } from 'react-router-dom'
+import hero from '../assets/img/hero.png'
 
 const Banner = () => {
  const history = useNavigate()
@@ -15,7 +16,7 @@ const Banner = () => {
    <div  className='trickery ' >
          <div className='hero-card'>
                 <div className='hero-div'>
-                  <img src="src\assets\img\download (3).png" alt=""  className='hero-clip'/>
+                  <img src={hero}alt=""  className='hero-clip'/>
                   <img src="src\assets\img\png-transparent-red-musical-symbol-music-symbol-red-note.png" alt=""  className='hero-clip2'/>
                 <img src="src\assets\img\hero.png" alt=""  className='hero' />  
 
