f8f70e453712ba2be5dc4da6297182aa90211c6c
bhig-sheydi <bhigsheydi@gmail.com>
2024-03-05 05:32:19 -0800

    TESTING STACK OVERFLOW

--- brekete-master/src/config/firebase.js
+++ brekete-master/src/config/firebase.js
@@ -9,12 +9,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAf4lIguJsxlw9OUixj2PNWPbRd8z7OLxk",
- 
+  authDomain: "breketemaster-dffb9.firebaseapp.com",
   projectId: "breketemaster-dffb9",
   storageBucket: "breketemaster-dffb9.appspot.com",
   messagingSenderId: "247309996534",
   appId: "1:247309996534:web:079b55a3ce712a4e85b7d7",
-  measurementId: "G-8HTDS4XEP3"
+  measurementId: "G-8HTDS4XEP3",
 };
 
 // Initialize Firebase
